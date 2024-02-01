7bcfe08040f91a1145095c9edb881e03b19696f6
sylvievdebrauwer <sylvie.debrauwer@gmail.com>
2024-02-01 19:28:13 +0100

    HomeController Created

--- Webshop/routes/web.php
+++ Webshop/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('item', function () {
-    return view('item');
+    return view("item");
 });
